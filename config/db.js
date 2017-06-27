6e39211185bfdc2365d374b5c2fdcc90fedd32c8
Shelby Solomon <shelby@shelbysolomon.net>
2017-06-27 15:13:09 -0500

    added config/

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+        url : 'mongodb://localhost:27017/?3t.connectTimeout=10000&3t.uriVersion=2&3t.connectionMode=direct&readPreference=primary&3t.socketTimeout=0&3t.connection.name=Local'
+    }
