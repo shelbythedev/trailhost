1553243c03950b3db59468ee7124e312330f19d1
Shelby Solomon <shelby@shelbysolomon.net>
2017-06-30 12:25:14 -0500

    added web server script and error handling

--- app.js
+++ app.js
@@ -7,4 +7,43 @@ let bodyParser = require('body-parser')
 // Initialize Express
 let app = express()
 
+// View engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+// Call in dependencies
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Index handler
+app.use((req, res, next) => {
+  res.status(200).send("Access to the TrailHost API via browser is not encouraged. Please visit https://github.com/shelbythedev/trailhost for more information.")
+})
+
+// 404 handler: catch and send to error handlers
+app.use((req, res, next) => {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Error handlers below
+
+// Development environment errors print stacktrace
+if (app.get('env') === 'development'){
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.json({error: err.message})
+  })
+}
+
+// Production environment will not reveal stacktrace to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.json({error: "Internal server error"})
+})
+
+
 module.exports = app
