259c8169bbf25336bea9806a79c7bf056be8fc98
Shelby Solomon <shelby@shelbysolomon.net>
2017-07-10 14:30:12 -0500

    updated db env

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-let dbConnection = process.env.MONGOLAB_URI || 'mongodb://localhost/trailhost'
+let dbConnection = process.env.MONGODB_URI || 'mongodb://localhost/trailhost'
 
 mongoose.connect(dbConnection, (err) => {
   if (err){
